b68c52fe173044a97323a8d4e38abb5e10f8d5b0
jcapoduri <jcapoduril@gmail.com>
2013-11-14 17:16:24 -0300

    testing restapi

---
+++ api/index.php
@@ -0,0 +1,31 @@
+<?php
+// CORS enable
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: X-Requested-With');
+
+// helper functions
+function getConnection() {
+	$dbhost="hostingbahia3.com.ar";
+	$dbuser="goldmoll_user";
+	$dbpass="a9GnAeJL";
+	$dbname="goldmoll_sindicato";
+	$handler = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+	return $habdler;
+}
+
+require 'Slim/Slim.php';
+
+\Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim();
+
+$app->get('/', function () use ($app){
+    echo "Undefined";
+});
+
+require 'test.php';
+
+$sql = getConnection();
+$app->run();
+
+?>
