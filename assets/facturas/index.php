70f8a12df80a7a8f0c0469f06818067115e7861a
Adrian <asanmartin@c2si.com.ar>
2013-11-18 19:19:01 -0300

    Factura v1

---
+++ assets/facturas/index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/css.css">
+</head>
+<body>
+<table width="800" align="center" cellpadding="0" cellspacing="4" class="maintable">
+  <tr>
+    <td align="center">
+      <table width="800" border="0" cellspacing="4" cellpadding="2">
+        <tr>
+          <td width="169" align="center" valign="center"><img src="img/sgs.png"></td>
+          <td align="right" valign="middle"><p class="textocomun"><img src="img/sgstexto.png" width="199" height="62"></p></td>
+          <td width="75" align="right" valign="top" style="padding-top: 20px;"><img src="img/fatida.png" width="60"></td>
+          <td width="330" align="right"><p class="textocomun">Nota de cr&eacute;dito para la cuenta Nro.: <b>24410761/75</b></p>
+            <p class="textocomun"><b>CBU 92172441076175</b></p>
+            <p class="textocomun"><b>Banco de la Naci&oacute;n Argentina</b><br>
+            Sucursal Don Bosco (9217)</p></td>
+        </tr>
+      </table>
+      <hr>
+    </td>
+  </tr>
+  <tr>
+    <td><table width="800" border="0" cellspacing="0" cellpadding="2" class="center">
+      <tr class="textoresaltado">
+        <td width="15%" align="center" valign="top">USUARIO</td>
+        <td width="20%" align="center" valign="top">CUIT</td>
+        <td width="45%" align="left" valign="top" class="center">RAZ&Oacute;N SOCIAL</td>
+        <td width="20%" align="right" valign="top" class="center">NRO. DE BOLETA</td>
+      </tr>
+      <tr class="textocomun">
+        <td align="center" valign="top"><strong>54321</strong></td>
+        <td align="center" valign="top"><strong>30-56652835-1</strong></td>
+        <td align="left" valign="top" class="center"><strong>SINDICATO GR&Aacute;FICO SURE&Ntilde;O</strong></td>
+        <td align="right" valign="top" class="center"><strong>000001</strong></td>
+      </tr>
+      <tr class="textoresaltado">
+        <td align="center" valign="top">PERSONAL</td>
+        <td align="center" valign="top">TOTAL REM. BRUTA</td>
+        <td align="center" valign="top">PER&Iacute;ODO</td>
+        <td colspan="2" align="center" valign="top">EMISI&Oacute;N</td>
+        </tr>
+      <tr class="textocomun">
+        <td align="center" valign="top"><strong>1</strong></td>
+        <td align="center" valign="top"><strong>9039.00</strong></td>
+        <td align="center" valign="top"><strong>07/13</strong></td>
+        <td colspan="2" align="center" valign="top"><strong>04/11/2013</strong></td>
+        </tr>
+    </table>
+      <table width="800" border="0" cellspacing="4" cellpadding="0">
+        <tr>
+          <td width="70%" valign="top"><table width="100%" border="0" cellpadding="2" cellspacing="0" class="textocomun">
+            <tr class="textoresaltado">
+              <td align="left">CONCEPTO</td>
+              <td align="right">IMPORTE</td>
+            </tr>
+            <tr>
+              <td>Cuota Sindical (2%)</td>
+              <td align="right">$180.74</td>
+            </tr>
+            <tr>
+              <td>Fondo Solidario (1%)</td>
+              <td align="right">$90.39</td>
+            </tr>
+            <tr>
+              <td>Cuota Mutual</td>
+              <td align="right">$50.00</td>
+            </tr>
+            <tr>
+              <td>Recargo Mora</td>
+              <td align="right">$9.63</td>
+            </tr>
+            <tr>
+              <td>Acuerdos</td>
+              <td align="right">$100.00</td>
+            </tr>
+            <tr>
+              <td>Pr&eacute;stamo</td>
+              <td align="right">$400.00</td>
+            </tr>
+            <tr class="textoresaltado">
+              <td class="fontplus" style="color: red;">Total Depositado</td>
+              <td align="right" class="textocomun fontplus" style="color: red;">$830.76</td>
+            </tr>
+          </table></td>
+          <td align="right" valign="bottom" class="textoresaltado">
+          	
+          	<div style="text-align:center;padding-left: 90px;">VENCIMIENTO<br><span class="textocomun" style="color: black;"><b>04/12/2013</b></span></div>
+          </td>
+        </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td><table width="100%" border="0" cellpadding="2" cellspacing="0" class="textocomun">
+      <tr>
+        <td align="right">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="resultado">Son Pesos: ocho cientos treinta con 76 centavos.</td>
+      </tr>
+      <tr>
+        <td align="right" style="padding-right: 10px;"><em><strong>Tal&oacute;n para el banco</strong></em><strong>.</strong></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center">
+    	<!-- AGREGAMOS CODIGO DE BARRAS GENERADO -->
+    	<P class="codigodebarra">0000543212441076175305665283510000010010713000830760412131</P>
+    </td>
+  </tr>
+</table>
+<br>
+
+<!-- ------------------------------------------------------------------------------------------------------- -->
+
+<table width="800" align="center" cellpadding="0" cellspacing="4" class="maintable">
+  <tr>
+    <td align="center">
+      <table width="800" border="0" cellspacing="4" cellpadding="2">
+        <tr>
+          <td width="169" align="center" valign="center"><img src="img/sgs.png"></td>
+          <td align="right" valign="middle"><p class="textocomun"><img src="img/sgstexto.png" width="199" height="62"></p></td>
+          <td width="75" align="right" valign="top" style="padding-top: 20px;"><img src="img/fatida.png" width="60"></td>
+          <td width="330" align="right"><p class="textocomun">Nota de cr&eacute;dito para la cuenta Nro.: <b>24410761/75</b></p>
+            <p class="textocomun"><b>CBU 92172441076175</b></p>
+            <p class="textocomun"><b>Banco de la Naci&oacute;n Argentina</b><br>
+            Sucursal Don Bosco (9217)</p></td>
+        </tr>
+      </table>     
+      <hr>
+    </td>
+  </tr>
+  <tr>
+    <td><table width="800" border="0" cellspacing="0" cellpadding="2" class="center">
+      <tr class="textoresaltado">
+        <td width="15%" align="center" valign="top">USUARIO</td>
+        <td width="20%" align="center" valign="top">CUIT</td>
+        <td width="45%" align="left" valign="top" class="center">RAZ&Oacute;N SOCIAL</td>
+        <td width="20%" align="right" valign="top" class="center">NRO. DE BOLETA</td>
+      </tr>
+      <tr class="textocomun">
+        <td align="center" valign="top"><strong>54321</strong></td>
+        <td align="center" valign="top"><strong>30-56652835-1</strong></td>
+        <td align="left" valign="top" class="center"><strong>SINDICATO GR&Aacute;FICO SURE&Ntilde;O</strong></td>
+        <td align="right" valign="top" class="center"><strong>000001</strong></td>
+      </tr>
+      <tr class="textoresaltado">
+        <td align="center" valign="top">PERSONAL</td>
+        <td align="center" valign="top">TOTAL REM. BRUTA</td>
+        <td align="center" valign="top">PER&Iacute;ODO</td>
+        <td colspan="2" align="center" valign="top">EMISI&Oacute;N</td>
+        </tr>
+      <tr class="textocomun">
+        <td align="center" valign="top"><strong>1</strong></td>
+        <td align="center" valign="top"><strong>9039.00</strong></td>
+        <td align="center" valign="top"><strong>07/13</strong></td>
+        <td colspan="2" align="center" valign="top"><strong>04/11/2013</strong></td>
+        </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td><table width="800" border="0" cellspacing="4" cellpadding="0">
+      <tr>
+        <td width="70%" valign="top"><table width="100%" border="0" cellpadding="2" cellspacing="0" class="textocomun">
+          <tr class="textoresaltado">
+            <td class="fontplus" style="color: red;">Total Depositado</td>
+            <td align="right" class="textocomun fontplus" style="color: red;">$830.76</td>
+          </tr>
+        </table>
+      </td>
+      <td align="right" valign="bottom" class="textoresaltado">
+        <div style="text-align:center;padding-left: 90px;">VENCIMIENTO<br><span class="textocomun" style="color: black;"><b>04/12/2013</b></span></div>
+      </td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td><table width="100%" border="0" cellpadding="2" cellspacing="0" class="textocomun">
+      <tr>
+        <td class="resultado">Son Pesos: ocho cientos treinta con 76 centavos.</td>
+      </tr>
+      <tr>
+        <td align="right"><em><strong>Tal&oacute;n para el depositante.</strong></em></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+<br>
+<center><form action="/" ><input name="Imprimir" type="button" class="boton" value="Imprimir" onclick="window.print();return false;"></form></center>
+</body>
+</html>
