a2b837313388cc0f87479e96425fde890f341fa3
Adrian <asanmartin@c2si.com.ar>
2013-11-26 00:06:49 -0300

    first state Model "sign in"

---
+++ assets/signin/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title></title>
+  <!-- Base stylesheet -->
+  <link rel="stylesheet" type="text/css" href="styles/default.css">
+</head>
+<body>
+	<div class="sign-left">
+		<span class="back-background">SGS</span>
+		<span class="front-background">Sindicato Grafico Sure&ntilde;o</span>
+	</div>
+	<div class="sign-right">
+		<div class="section">
+			<div class="label">CUIT</div>
+			<div class="textbox"><input type="text" maxlength="11"></div>
+			<div class="label">Contrase&ntilde;a</div>
+			<div class="textbox"><input type="password" maxlength="25"></div>
+			<div class="label iniciar">Iniciar Sesi&oacute;n</div>
+		</div>
+	</div>
+</body>
+</html>
