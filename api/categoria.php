bccda4f69c375b68bb948587927041f2bb2964d8
Adrian <sanmartin.adrian@gmail.com>
2013-12-10 17:28:21 -0500

    Second commit c9

--- api/categoria.php
+++ api/categoria.php
@@ -1,58 +1,120 @@
 <?php
 
-require 'Slim/Slim.php';
+//require 'Slim/Slim.php';
 
-$app = new Slim();
+//$app = new Slim();
 
-$app->get('/categorias', 'getCategorias');
-$app->get('/categorias/:id',	'getCategoria');
-$app->get('/categorias/search/:query', 'findByName');
-$app->post('/categorias', 'addCategoria');
-$app->put('/categorias/:id', 'updateCategoria');
-$app->delete('/categorias/:id',	'deleteCategoria');
+$app->get('/categorias', function() use ($app){
 
-$app->run();
+	global $sql;
+	$query = "SELECT * FROM categorias WHERE deleted = 0";
+	$resource = $sql->query($query);
 
-function getCategorias() {
+	$response = $app->response();
+	$response['Content-Type'] = 'application/json';
 
-	$sql = "select * FROM categoria ORDER BY name";
-	try {
-		$db = getConnection();
-		$stmt = $db->query($sql);  
-		$categorias = $stmt->fetchAll(PDO::FETCH_OBJ);
-		$db = null;
-		echo json_encode($categorias);
-	} catch(PDOException $e) {
-		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+	if ($resource === false) {
+		$app->response()->status(400);
+	} else {
+		$ret_array = array();
+		while($row = $resource->fetch_assoc()){
+			$ret_array[] = $row;
+		};
+		$response->write(json_encode($ret_array));
+	};
+});
+
+$app->get('/categorias/:id', function($id) use ($app){
+
+	global $sql;
+	$query = "SELECT * FROM categorias WHERE deleted = 0 AND id = " . $id;
+	$resource = $sql->query($query);
+
+	$response = $app->response();
+	$response['Content-Type'] = 'application/json';
+
+	if ($resource === false) {
+		response()->status(400);
+	} else {
+		$obj = $resource->fetch_assoc();
+
+		if (is_null($obj)) {
+			response()->status(400);
+		} else {
+			$response->write(json_encode($obj));
+		};
+	};
+});
+
+$app->post('/categorias', function() use ($app){
+
+	global $sql;
+	$json = json_decode($app->request()->getBody(), true);
+
+	if (is_null($json)) {
+		response()->status(400);
+		return;
 	}
-}
+	$sql->autocommit(FALSE);
 
-function getCategoria($id) {
+	$id = $bluesystem->createObject("categoria", $json);
+	
+	if ($id) {
+		$bluesystem->handler->commit();
+		$app->response()->write($id);
+	} else {
+		response()->status(400);
+	};
+});
 
-	$sql = "SELECT * FROM categoria WHERE id=:id";
-	try {
-		$db = getConnection();
-		$stmt = $db->prepare($sql);  
-		$stmt->bindParam("id", $id);
-		$stmt->execute();
-		$categorias = $stmt->fetchObject();  
-		$db = null;
-		echo json_encode($categorias); 
-	} catch(PDOException $e) {
-		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
-	}
-}
+$app->put('/categorias/:id', function ($id) use ($app){
 
-function addCategoria() {
-}
+	global $sql;
+	$json = json_decode($app->request()->getBody(), true);
+	$query = "SELECT * FROM categorias WHERE deleted = 0 AND id = " . $id;
+	$resource = $sql->query($query);
 
-function updateCategoria($id) {
-}
+	$response = $app->response();
+	$response['Content-Type'] = 'application/json';
 
-function deleteCategoria($id) {
-}
+	$sql->autocommit(FALSE);
+	$id = $bluesystem->createObject("trabajador", $json);
 
-function findByName($query) {
-}
+	if ($resource === false && is_null($json)) {
+		response()->status(400);
+	} else {
+		$obj = $resource->fetch_assoc();
+
+		if (is_null($obj)) {
+			response()->status(400);
+		} else {
+			$bluesystem->handler->commit();
+			$app->response()->write($id);
+		};
+	};
+});
+
+$app->delete('/categorias/:id', function ($id) use ($app){
+	
+	global $sql;
+	$query = "SELECT * FROM categorias WHERE deleted = 0 AND id = " . $id;
+	$resource = $sql->query($query);
+
+	$response = $app->response();
+	$response['Content-Type'] = 'application/json';
+
+	if ($resource === false) {
+		response()->status(400);
+	} else {
+		$obj = $resource->fetch_assoc();
+
+		if (is_null($obj)) {
+			response()->status(400);
+		} else {
+			// cambiar el flag
+			$response->write(json_encode($obj));
+		};
+	};
+});
 
 ?>
