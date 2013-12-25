a9143da10ef583aef9ba8af2ed05b877a71230d2
Adrian <sanmartin.adrian@gmail.com>
2013-12-24 19:54:03 -0500

    Slim update

--- api/index.php
+++ api/index.php
@@ -3,20 +3,27 @@
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: X-Requested-With');
+ini_set('display_errors', 1);
 
+// SLIM Framework, to provide REST capability and scalfolding
 require 'Slim/Slim.php';
+// small propietary ORM
 require 'nd/nd.php';
 
+// SLIM setting up
 \Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim();
 
-// Instantiate a new Slim application
-$app = new \Slim\Slim(array(
-    'debug' => true,
-    'log.level' => \Slim\Log::DEBUG
-));
+// Nd settings up
+$config_file = 'sindicato.json';
+$config_data = file_get_contents($config_file);
+$config_json = json_decode($config_data, true);
+$system = new \nd\neodymium($config_json);
 
 $app->get('/', function () use ($app){
-    $app->response()->write('hola');
+    $response = $app->response();
+    $response['Content-Type'] = 'application/json';
+    $response->write('sdkjfhjjkfads');
 });
 
 require 'users.php';
