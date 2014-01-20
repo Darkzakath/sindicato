fd00185c1d28f34ac182d32ec137cbefd864919f
jcapoduri <jcapoduri@gmail.com>
2014-01-20 13:15:11 -0500

    branch fixing

---
+++ js/models/user.model.js
@@ -0,0 +1,42 @@
+define (['backbone'], function(backbone){
+	return backbone.Model.extend({
+		defaults:{
+			username: '',
+			password: '',
+			email: ''
+		},
+		url: 'api/my',
+        login: function (loginObj) {
+            $.ajax({
+                url: 'api/login',
+                type: "POST",
+                data: JSON.stringify(loginObj),
+                processData: false,
+                contentType: "application/json; charset=utf-8",
+                success: function(response) {
+                    $.ajaxSetup({
+                        headers: { 'ACCESS_TOKEN': response }
+                    });
+                    this.set('loged', true);
+                    this.set('token', response);
+                    $.cookie('ACCESS_TOKEN', response);
+                    this.setUp();
+                }.bind(this),
+                error: function() {
+                        bootbox.dialog({
+                          message: "Error al ingresar, compruebe usuario y password",
+                          title: "Error al ingreso",
+                          buttons: {
+                            main: {
+                              label: "Ok",
+                              className: "btn-primary",
+                              callback: function() {}
+                            }
+                          }
+                        });
+                    }
+            })
+        }
+	});
+
+});
