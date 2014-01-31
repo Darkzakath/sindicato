942f6d16a0fccb73a3e5a297139aad406c1dbff6
Adrian <sanmartin.adrian@gmail.com>
2014-01-30 19:25:49 -0500

    UI Left User

---
+++ assets/UI left user/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="style/css.css">
+	</head>
+	<body>
+		<ul>
+			<li>Mis Datos</li>
+			<li>"Nombre de empresa"</li>
+			<li>CUIT: xx-xxxxxxxx-x</li>
+			<li>Cantidad de personal: xx</li>
+			<li>Direccion: xxxxxxxxxxxxx</li>
+			<li>Telefono: xxxxxxxxxxxxxx</li>
+			<li>Estudio contable: xxxxxxxxxxx</li>
+		</ul>
+		<br>
+		<ul>
+			<li>Imprimer boleta</li>
+			<li>Historial de boletas</li>
+			<li>Ver nomina de trabajadores</li>
+			<li>Escala de salarios basicos</li>
+		</ul>
+	</body>
+</html>
